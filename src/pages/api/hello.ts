fc618c440940e80596de9503f015c144d3ccabb2
Dipesh Subedi <dipeshsubedi@Dipeshs-MacBook-Air.local>
2023-07-18 18:18:04 +0545

    init

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
+interface Data {
+  id: number;
+  amount: string;
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | string>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const shouldSendTransaction = Math.random() < 0.6; // 80% chance of sending a transaction
+
+  if (shouldSendTransaction) {
+    const id = Math.floor(Math.random() * 1000) + 1; // Generate a random id between 1 and 1000
+    const amount = (Math.random() * 100).toFixed(2); // Generate a random amount between 0 and 100 with 2 decimal places
+    const data: Data = {
+      id,
+      amount,
+    };
+    res.status(200).json(data);
+  } else {
+    res.send("No Transaction"); // No content to send (no transaction)
+  }
 }
